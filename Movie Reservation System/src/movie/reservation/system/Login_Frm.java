6f3839eb649f2d2ad4f6835b27283ff641d7f1ab
nadinCodeHat <nadinpethiyagoda4@gmail.com>
2021-04-03 11:41:11 +0530

    Updated Login

--- Movie Reservation System/src/movie/reservation/system/Login_Frm.java
+++ Movie Reservation System/src/movie/reservation/system/Login_Frm.java
@@ -31,12 +31,12 @@ public class Login_Frm extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
-        jLabel4 = new javax.swing.JLabel();
+        forgotPassword_link = new javax.swing.JLabel();
         jTextField2 = new javax.swing.JTextField();
         jLabel5 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        login_Btn = new javax.swing.JButton();
         jLabel6 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
+        createAccount_link = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(255, 255, 255));
@@ -64,21 +64,25 @@ public class Login_Frm extends javax.swing.JFrame {
 
         jTextField1.setBackground(new java.awt.Color(21, 20, 31));
         jTextField1.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jTextField1.setForeground(new java.awt.Color(255, 255, 255));
         jTextField1.setHorizontalAlignment(javax.swing.JTextField.LEFT);
         jTextField1.setToolTipText("");
         jTextField1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(235, 16, 42)));
+        jTextField1.setCaretColor(new java.awt.Color(255, 255, 255));
         login_panel.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 200, 220, 30));
 
-        jLabel4.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jLabel4.setForeground(new java.awt.Color(235, 16, 42));
-        jLabel4.setText("Forgot Password?");
-        login_panel.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 240, -1, 20));
+        forgotPassword_link.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        forgotPassword_link.setForeground(new java.awt.Color(235, 16, 42));
+        forgotPassword_link.setText("Forgot Password?");
+        login_panel.add(forgotPassword_link, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 240, -1, 20));
 
         jTextField2.setBackground(new java.awt.Color(21, 20, 31));
         jTextField2.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jTextField2.setForeground(new java.awt.Color(255, 255, 255));
         jTextField2.setHorizontalAlignment(javax.swing.JTextField.LEFT);
         jTextField2.setToolTipText("");
         jTextField2.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(235, 16, 42)));
+        jTextField2.setCaretColor(new java.awt.Color(255, 255, 255));
         login_panel.add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 270, 220, 30));
 
         jLabel5.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
@@ -86,21 +90,21 @@ public class Login_Frm extends javax.swing.JFrame {
         jLabel5.setText("Password");
         login_panel.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 240, -1, 20));
 
-        jButton1.setBackground(new java.awt.Color(235, 16, 42));
-        jButton1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(255, 255, 255));
-        jButton1.setText("Login");
-        login_panel.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 340, 220, 35));
+        login_Btn.setBackground(new java.awt.Color(235, 16, 42));
+        login_Btn.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        login_Btn.setForeground(new java.awt.Color(255, 255, 255));
+        login_Btn.setText("Login");
+        login_panel.add(login_Btn, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 340, 220, 35));
 
         jLabel6.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jLabel6.setForeground(new java.awt.Color(255, 255, 255));
         jLabel6.setText("New User?");
         login_panel.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 390, -1, 20));
 
-        jLabel7.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jLabel7.setForeground(new java.awt.Color(235, 16, 42));
-        jLabel7.setText("Create Account");
-        login_panel.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 390, -1, 20));
+        createAccount_link.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        createAccount_link.setForeground(new java.awt.Color(235, 16, 42));
+        createAccount_link.setText("Create Account");
+        login_panel.add(createAccount_link, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 390, -1, 20));
 
         getContentPane().add(login_panel, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 0, 310, 500));
 
@@ -144,16 +148,16 @@ public class Login_Frm extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel createAccount_link;
+    private javax.swing.JLabel forgotPassword_link;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
+    private javax.swing.JButton login_Btn;
     private javax.swing.JPanel login_panel;
     // End of variables declaration//GEN-END:variables
 }
